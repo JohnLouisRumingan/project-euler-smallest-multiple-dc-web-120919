9fc6202c0e6b8daf63dd30abd32c5462ab108e8c
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-23 09:57:26 -0400

    Started

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,25 @@
-# Implement your procedural solution here!
+
+
+def smallest_multiple(num)
+
+    num_array = [*1..num]
+
+    lowest_num = 1;
+
+    begin 
+
+        t_f = true;
+
+        for i in num_array
+
+            if lowest_num % i != 0;
+                t_f = false;
+            end 
+        end 
+
+        lowest_num += 1;
+    until t_f = true; 
+
+    
+    return lowest_num 
+end 
